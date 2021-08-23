09b3c896687901f7234afd232996d672e52e576f
naman <naman.jain@btech2015.iitgn.ac.in>
2021-08-23 21:16:42 +0530

    wip: update pdf report content

--- src/generate_pdf_report.py
+++ src/generate_pdf_report.py
@@ -1,9 +1,10 @@
 from fpdf import FPDF
 import os
 from PIL import Image
+from src import fetch_data
 
 
-def generate_pdf(src_dir, aoi_name, date, n_clusters, out_pdf_path):
+def generate_pdf(src_dir, aoi_path, date, n_clusters, out_pdf_path):
     rgb_path = os.path.join(src_dir, "rgb.png")
     ndvi_path = os.path.join(src_dir, "ndvi_vis.png")
     ndvi_classes_path = os.path.join(src_dir, "ndvi_classes_vis.png")
@@ -19,13 +20,23 @@ def generate_pdf(src_dir, aoi_name, date, n_clusters, out_pdf_path):
     pdf.add_page()
     pdf.image(pdf_background_path, x=0, y=0, w=210, h=297, type="", link="")
     pdf.rect(5.0, 5.0, 200.0, 287.0)
-    pdf.set_font("helvetica", "B", 10)
+    pdf.set_font("helvetica", "B", 15)
     pdf.set_text_color(0, 0, 0)
-
+    aoi_name = aoi_path.split("/")[-1].split(".")[0]
     pdf.text(10, 10, "Report on Vegetation health for: {}".format(aoi_name))
-    pdf.text(10, 15, "Date: {}".format(date))
-    pdf.text(10, 18, "RGB image")
-    pdf.image(rgb_path, x=45, y=30, w=120, h=90)
+    pdf.text(10, 20, "Date: {}".format(date))
+
+    pdf.set_font("helvetica", "B", 12)
+    aoi_bounds = fetch_data.get_vector_bbox(aoi_path)
+    pdf.text(10, 40, "AOI bounds:")
+    pdf.set_font("helvetica", "B", 10)
+    pdf.text(13, 50, "min_x: {}".format(round(aoi_bounds[0], 5)))
+    pdf.text(13, 55, "min_y: {}".format(round(aoi_bounds[1], 5)))
+    pdf.text(13, 60, "max_x: {}".format(round(aoi_bounds[2], 5)))
+    pdf.text(13, 65, "max_y: {}".format(round(aoi_bounds[3], 5)))
+    pdf.set_font("helvetica", "B", 12)
+    pdf.text(10, 100, "RGB image")
+    pdf.image(rgb_path, x=45, y=115, w=120, h=90)
 
     pdf.add_page()
     pdf.image(pdf_background_path, x=0, y=0, w=210, h=297, type="", link="")
